0a068e13efc5637c45f7f23b97f31a085799983e
Zordon <65111609+Zordon1337@users.noreply.github.com>
2024-02-13 15:38:39 +0100

    Add project files.

---
+++ BanchoTroller/Program.cs
@@ -0,0 +1,257 @@
+﻿
+using BanchoTroller.Structs;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Globalization;
+
+
+public class Server
+{
+    public class Writer : BinaryWriter
+    {
+        public Writer(Stream s) : base(s) { }
+        public override void Write(string value)
+        {
+            if (value != null)
+            {
+                if (value.Length == 0)
+                {
+                    this.Write(new byte[] { 0x00 });
+                    return;
+                }
+            }
+
+            try
+            {
+                this.Write((byte)11);
+                base.Write(value);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+    public static string ReadStringFromStream(TcpClient client)
+    {
+        byte[] buffer = new byte[4096];
+        var stream = client.GetStream();
+
+        if (stream.CanRead)
+        {
+            string receivedData = null;
+
+            
+            var tsk = Task.Run(() =>
+            {
+                while (true)
+                {
+                    
+                    if (stream.DataAvailable)
+                    {
+                        int bytesReceived = stream.Read(buffer, 0, buffer.Length);
+                        receivedData += Encoding.UTF8.GetString(buffer, 0, bytesReceived);
+                    }
+                    else
+                    {
+                        
+                        break;
+                    }
+                }
+            });
+
+            Task.WaitAll(tsk);
+
+            return receivedData;
+        }
+
+        return null;
+    }
+
+    
+    public static void Annouce(TcpClient user, string message)
+    {
+        MemoryStream ms = new MemoryStream();
+        Writer writer = new Writer(ms);
+        writer.Write(message);
+        writer.Flush();
+        SendPacket(25,ms,user);
+    }
+    public static void Main(string[] args)
+    {
+        TcpListener srv = new TcpListener(13381);
+
+        srv.Start();
+        while(true)
+        {
+            var client = srv.AcceptTcpClient();
+            MemoryStream ms = new MemoryStream();
+            Writer bw = new Writer(ms);
+            
+            try
+            {
+                string data = ReadStringFromStream(client);
+                string[] lines = new string[] { };
+                if (data != null)
+                {
+                     lines = data.Split('\n');
+                }
+                if (lines.Length >= 2)
+                {
+                    string username = lines[0]?.Trim();
+                    string password = lines[1]?.Trim();
+                    string version = lines[2].Split('|')[0];
+                    Console.WriteLine($"{username} {password}");
+                    string reply = new WebClient().DownloadString($"http://osu.zndev.xyz:8080/web/osu-login.php?username={username}&password={password}");
+                    if (reply == "1")
+                    {
+                        Console.WriteLine("correct");
+                        bw.Write(1);
+                        bw.Flush();
+                        SendPacket(5, ms, client);
+                        ms = new MemoryStream();
+                        
+                        try
+                        {
+                            string userdata = new WebClient().DownloadString("http://osu.zndev.xyz:8080/web/osu-statoth.php?u=" + username);
+                            string[] lines2 = userdata.Split('|');
+                            long totalscore = long.Parse(lines2[0]);
+                            
+                            float accuracy = Single.Parse(lines2[1], CultureInfo.InvariantCulture);
+                            int rank = int.Parse(lines2[4]);
+                            string pfp = lines2[5];
+                            Console.WriteLine($"{username} just logged with {version}");
+                            bStatusUpdate su = new bStatusUpdate(bStatus.Idle, false, "", "", 0, Mods.None, PlayModes.OsuStandard);
+                            Writer bw2 = new Writer(ms);
+                            Int32 id = 1;
+                            bw2.Write(id); // id
+                            bw2.Write((byte)Completeness.Statistics); // completeness
+                            su.WriteToStream(bw2); // status
+                            bw2.Write((long)totalscore); // Ranked score
+                            bw2.Write((float)accuracy); // Accuracy
+                            bw2.Write(0); // Playcount
+                            bw2.Write((long)totalscore); // Total Score
+                            bw2.Write(rank); // Rank
+                            SendPacket(12, ms, client);
+                            bw.Write("#osu");
+                            bw.Flush();
+                            SendPacket(64, ms, client);
+                            new Thread(() => PlayerThread(client, username, version, id)).Start();
+                            
+                        } catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("not correct");
+                        bw.Write(-1);
+                        bw.Flush();
+                        SendPacket(5, ms, client);
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Blyaaaat");
+                }
+            } catch
+            {
+
+            }
+
+            
+           
+            
+        }
+    }
+    
+    public static void PlayerThread(TcpClient client, string username, string version, int userid)
+    {
+        long oldscore = 0;
+        try
+        {
+            Annouce(client, "Welcome to BanchoTroller \nosu2007srv proxy for \nclients using TCP");
+        } catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        new Thread(() =>
+        {
+            while (true)
+            {
+                SendEmptyPacket(8, client);
+            }
+        });
+        while (true)
+        {
+            if(client.Connected)
+            {
+                var stream = client.GetStream();
+                MemoryStream ms = new MemoryStream();
+                Writer w2 = new Writer(ms);
+                BinaryReader br = new BinaryReader(stream);
+                try
+                {
+                    switch (br.ReadInt16())
+                    {
+
+                        case (int)RequestType.Osu_RequestStatusUpdate:
+                            {
+                                string userdata = new WebClient().DownloadString("http://osu.zndev.xyz:8080/web/osu-statoth.php?u=" + username);
+                                string[] lines2 = userdata.Split('|');
+                                long totalscore = long.Parse(lines2[0]);
+                                float accuracy = Single.Parse(lines2[1], CultureInfo.InvariantCulture);
+                                int rank = int.Parse(lines2[4]);
+                                string pfp = lines2[5];
+                                bStatusUpdate su = new bStatusUpdate(bStatus.Idle, false, "", "", 0, Mods.None, PlayModes.OsuStandard);
+                                w2.Write(userid);
+                                w2.Write((byte)Completeness.Statistics);
+                                su.WriteToStream(w2);
+                                w2.Write((long)totalscore);
+                                w2.Write((float)accuracy);
+                                w2.Write(0);
+                                w2.Write((long)totalscore);
+                                w2.Write(rank);
+                                SendPacket(12, ms, client);
+                                Console.WriteLine("Got Packet");
+                                break;
+                            }
+                    }
+                } catch { }
+                
+            } else
+            {
+                //return;
+            }
+        }
+    }
+    public static void SendPacket(int packet, MemoryStream ms, TcpClient client)
+    {
+        try
+        {
+            Writer bw = new Writer(client.GetStream());
+            bw.Write((ushort)packet);
+            bw.Write(false);
+            bw.Write((uint)ms.Length);
+            bw.Write(ms.ToArray());
+            bw.Flush();
+        } catch { }
+        
+    }
+    public static void SendEmptyPacket(int packet, TcpClient c )
+    {
+        try
+        {
+            Writer bw = new Writer(c.GetStream());
+            bw.Write((ushort)packet);
+            bw.Write(false);
+            bw.Write(0);
+            bw.Flush();
+        } catch { }
+    }
+}
---
+++ BanchoTroller/Structs.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using static Server;
+
+namespace BanchoTroller.Structs
+{
+    public class bUserStats
+    {
+        public bUserStats(int userId, string username, long rankedScore, float accuracy, int playcount, long totalScore, int rank, string avatarFilename, bStatusUpdate status, int timezone, string location, Permissions permission)
+        {
+            this.userId = userId;
+            this.username = username;
+            this.rankedScore = rankedScore;
+            this.accuracy = accuracy;
+            this.playcount = playcount;
+            this.totalScore = totalScore;
+            this.rank = rank;
+            this.avatarFilename = avatarFilename;
+            this.status = status;
+            this.timezone = timezone;
+            this.location = location;
+            this.permission = permission;
+        }
+
+        
+
+
+        public void WriteToStream(MemoryStream ms)
+        {
+            this.WriteToStream(new Writer(ms), false);
+        }
+
+        public void WriteToStream(Writer sw, bool forceFull)
+        {
+            sw.Write(this.userId);
+            sw.Write((byte)this.completeness);
+            this.status.WriteToStream(sw);
+            if (this.completeness > Completeness.StatusOnly || forceFull)
+            {
+                sw.Write(this.rankedScore);
+                sw.Write(this.accuracy);
+                sw.Write(this.playcount);
+                sw.Write(this.totalScore);
+                sw.Write((ushort)this.rank);
+            }
+            if (this.completeness == Completeness.Full || forceFull)
+            {
+                sw.Write(this.username);
+                sw.Write(this.avatarFilename);
+                sw.Write((byte)(this.timezone + 24));
+                sw.Write(this.location);
+                sw.Write((byte)this.permission);
+            }
+            
+        }
+
+        
+
+        public float accuracy;
+
+        public string avatarFilename;
+
+        public Completeness completeness;
+
+        public int level;
+
+        public string location;
+
+        private readonly Permissions permission;
+
+        public int playcount;
+
+        public int rank;
+
+        public long rankedScore;
+
+        public bStatusUpdate status;
+
+        public int timezone;
+
+        public long totalScore;
+
+        public int userId;
+
+        public string username;
+    }
+    public enum Permissions
+    {
+        None = 0,
+        Normal = 1,
+        BAT = 2,
+        Subscriber = 4
+    }
+    public enum bStatus
+    {
+        Idle,
+        Afk,
+        Playing,
+        Editing,
+        Modding,
+        Multiplayer,
+        Watching,
+        Unknown,
+        Testing,
+        Submitting,
+        StatsUpdate,
+        Paused,
+        Lobby
+    }
+    public enum RequestType
+    {
+        Osu_SendUserStatus,
+        Osu_SendIrcMessage,
+        Osu_Exit,
+        Osu_RequestStatusUpdate,
+        Osu_Pong,
+        Bancho_LoginReply,
+        Bancho_CommandError,
+        Bancho_SendIrcMessage,
+        Bancho_Ping,
+        Bancho_HandleIrcChangeUsername,
+        Bancho_HandleIrcQuit,
+        Bancho_HandleIrcJoin,
+        Bancho_HandleOsuUpdate,
+        Bancho_HandleOsuQuit,
+        Bancho_SpectatorJoined,
+        Bancho_SpectatorLeft,
+        Bancho_SpectateFrames,
+        Osu_StartSpectating,
+        Osu_StopSpectating,
+        Osu_SpectateFrames,
+        Bancho_VersionUpdate,
+        Osu_ErrorReport,
+        Osu_CantSpectate,
+        Bancho_SpectatorCantSpectate,
+        Bancho_GetAttention,
+        Bancho_Announce,
+        Osu_SendIrcMessagePrivate,
+        Bancho_MatchUpdate,
+        Bancho_MatchNew,
+        Bancho_MatchDisband,
+        Osu_LobbyPart,
+        Osu_LobbyJoin,
+        Osu_MatchCreate,
+        Osu_MatchJoin,
+        Osu_MatchPart,
+        Bancho_LobbyJoin,
+        Bancho_LobbyPart,
+        Bancho_MatchJoinSuccess,
+        Bancho_MatchJoinFail,
+        Osu_MatchChangeSlot,
+        Osu_MatchReady,
+        Osu_MatchLock,
+        Osu_MatchChangeSettings,
+        Bancho_FellowSpectatorJoined,
+        Bancho_FellowSpectatorLeft,
+        Osu_MatchStart,
+        AllPlayersLoaded,
+        Bancho_MatchStart,
+        Osu_MatchScoreUpdate,
+        Bancho_MatchScoreUpdate,
+        Osu_MatchComplete,
+        Bancho_MatchTransferHost,
+        Osu_MatchChangeMods,
+        Osu_MatchLoadComplete,
+        Bancho_MatchAllPlayersLoaded,
+        Osu_MatchNoBeatmap,
+        Osu_MatchNotReady,
+        Osu_MatchFailed,
+        Bancho_MatchPlayerFailed,
+        Bancho_MatchComplete,
+        Osu_MatchHasBeatmap,
+        Osu_MatchSkipRequest,
+        Bancho_MatchSkip,
+        Bancho_Unauthorised,
+        Osu_ChannelJoin,
+        Bancho_ChannelJoinSuccess,
+        Bancho_ChannelAvailable,
+        Bancho_ChannelRevoked,
+        Bancho_ChannelAvailableAutojoin,
+        Osu_BeatmapInfoRequest,
+        Bancho_BeatmapInfoReply,
+        Osu_MatchTransferHost,
+        Bancho_LoginPermissions,
+        Bancho_FriendsList,
+        Osu_FriendAdd,
+        Osu_FriendRemove,
+        Bancho_ProtocolNegotiation
+    }
+    public enum Mods
+    {
+
+        None,
+        NoFail,
+        Easy,
+        NoVideo = 4,
+        Hidden = 8,
+        HardRock = 16,
+        SuddenDeath = 32,
+        DoubleTime = 64,
+        Relax = 128,
+        HalfTime = 256,
+        Taiko = 512
+    }
+    public enum Completeness
+    {
+        StatusOnly,
+        Statistics,
+        Full
+    }
+    public enum SlotStatus
+    {
+        Open = 1,
+        Locked = 2,
+        NotReady = 4,
+        Ready = 8,
+        NoMap = 16,
+        Playing = 32,
+        Complete = 64,
+        HasPlayer = 124
+    }
+    public enum MatchTypes
+    {
+        Standard,
+        Powerplay
+    }
+    public enum PlayModes
+    {
+        OsuStandard,
+        Taiko,
+        CatchTheBeat
+    }
+    public class bStatusUpdate
+    {
+        public bStatusUpdate(bStatus status, bool beatmapUpdate, string statusText, string songChecksum, int beatmapId, Mods mods, PlayModes playMode)
+        {
+            this.status = status;
+            this.beatmapUpdate = beatmapUpdate;
+            this.beatmapChecksum = songChecksum;
+            this.statusText = statusText;
+            this.currentMods = mods;
+            this.playMode = playMode;
+            this.beatmapId = beatmapId;
+        }
+
+        public bStatusUpdate(Stream s)
+        {
+            if (s.CanRead)
+            {
+                try
+                {
+                    BinaryReader sr = new BinaryReader(s);
+                    this.status = (bStatus)sr.ReadByte();
+                    this.beatmapUpdate = sr.ReadBoolean();
+                    if (!this.beatmapUpdate)
+                    {
+                        return;
+                    }
+                    this.statusText = sr.ReadString();
+                    this.beatmapChecksum = sr.ReadString();
+                    this.currentMods = (Mods)sr.ReadUInt16();
+                    this.playMode = (PlayModes)sr.ReadByte();
+                    this.beatmapId = sr.ReadInt32();
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+        }
+        public void WriteToStream(Writer sw)
+        {
+            sw.Write((byte)this.status);
+            sw.Write(this.beatmapUpdate);
+            if (!this.beatmapUpdate)
+            {
+                return;
+            }
+            sw.Write(this.statusText);
+            sw.Write(this.beatmapChecksum);
+            sw.Write((ushort)this.currentMods);
+            sw.Write((byte)this.playMode);
+            sw.Write(this.beatmapId);
+            
+        }
+
+        
+        public string beatmapChecksum;
+
+        public int beatmapId;
+
+        public bool beatmapUpdate;
+
+        public Mods currentMods;
+
+        public PlayModes playMode;
+
+        public bStatus status;
+
+        public string statusText;
+    }
+    
+}
